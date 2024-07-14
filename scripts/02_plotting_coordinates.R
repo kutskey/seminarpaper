fd3a16ea35f0e7bda4cf06b8148f2d987674c124
Silvan Hess <silvan.hess@stud.unilu.ch>
2024-07-14 15:49:32 +0200

    add ged_coordinates to map

--- scripts/02_plotting_coordinates.R
+++ scripts/02_plotting_coordinates.R
@@ -7,20 +7,28 @@ rm(list=ls())
 library(tidyverse)
 library(sf)
 library(tmap)
+library(leaflet)
 
 # Lade fao_data.rds in R
 fao_data <- read_rds("data_prep/fao_data.rds")
+ged_africa <- read_rds("data_prep/ged_africa.rds")
 
 # Erstelle die Karte mit sf und tmap ----
 
 # konvertiere in sf objekt
 
-data_sf <- st_as_sf(fao_data, coords = c("longitude", "latitude"), crs = 4326)
+fao_sf <- st_as_sf(fao_data, coords = c("longitude", "latitude"), crs = 4326)
+ged_sf <- st_as_sf(ged_africa, coords = c("longitude", "latitude"), crs = 4326)
+
+# Switch tmap mode to "view" for interactive maps
+tmap_mode("view")
 
 # erstelle die Karte
-tm <- tm_shape(data_sf) +
-  tm_dots(size = 0.1, col = "red") +
-  tm_basemap("OpenStreetMap")
+map <- tm_basemap("OpenStreetMap") +
+  tm_shape(fao_sf) + 
+  tm_dots(col = "red", size = 0.1) +
+  tm_shape(ged_sf) + 
+  tm_dots(col = "blue", size = 0.1)
 
-# speichere die Karte als html
-tmap_save(tm, filename = "plots/map_of_dams.html")
+# save map as html
+tmap_save(map, "plots/map.html")
